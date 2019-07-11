f8db1fb1f7be940491dfd06c752429550cb8a700
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 12:34:46 -0700

    scientist/idea update

--- src/Scientist.java
+++ src/Scientist.java
@@ -4,5 +4,7 @@ public class Scientist {
     public int id;
     public int age;
     public HashMap<Integer, HashMap<String, Integer>> aspects;
-
+    public double learning_speed;
+    public double max_var;
+    public double lam_var;
 }
