3394381c6a0cb8e3e76a2956ee6393455d51f2af
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 14:15:26 -0700

    model step update

--- src/Model.java
+++ src/Model.java
@@ -17,10 +17,12 @@ public class Model {
 		
     }
 	
-	public ArrayList<Scientist> scientists;
+	public ArrayList<Scientist> scientist_list;
     
-	public ArrayList<Idea> ideas;
+	public ArrayList<Idea> idea_list;
 	
+	/* Public variables that can be accessed across methods, updated by parameters from
+	 * config (might make sense to just remove the config layer) */
 	public int num_sci;
 	public int time_periods;
 	public int ideas_per_time;
@@ -30,17 +32,34 @@ public class Model {
 	public int idea_max;
 	public int start_effort_mean;
 	
+	// Model Step
     public void step() {
-    	for(int i = 0; i < num_sci; i++) {
-    		Scientist scientist = new Scientist();
-    		scientist.idea_max_var = getRandomNumber(idea_max);
+    	age_scientists();
+    	birth_new_scientists();
+    }
+    
+    public void age_scientists() {
+    	for(int i = 0; i < scientist_list.size() + 1; i++) {
+    		
     	}
     }
     
-    public int getRandomNumber(int average) {
+    public void birth_new_scientists() {
+    	for(int i = 0; i < num_sci; i++) {
+    		Scientist scientist = new Scientist();
+    		scientist.age = 0;
+    		// unclear if the following inputs are right, but structure is correct
+    		scientist.idea_max_var = getRandomNumber(idea_max);
+    		scientist.idea_mean_var = getRandomNumber(idea_mean);
+    		scientist.learning_speed = getRandomNumber(k_mean);
+    		scientist_list.add(scientist);
+    	}
+    }
+    
+    // Generates a random number within a range of [0, max]
+    public int getRandomNumber(int max) {
 		double x = Math.random();
 		int min = 0;
-		int max = average * 2;
 		int num = (int)(x * ((max - min) + 1)) + min;
 		return num;
 	}
