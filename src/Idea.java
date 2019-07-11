6efa1caedb8001f44bfe1a276d455ec72a02c075
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 12:35:04 -0700

    idea update

--- src/Idea.java
+++ src/Idea.java
@@ -1,8 +1,8 @@
 public class Idea {
     // instrinsic variables
-    public int lam; // aka inflection point
-    public int max; // multiple, curve goes from 0 - M
-    public int k; // entry cost/barrier to enter an idea
+    public int idea_mean; // aka inflection point
+    public int idea_max; // multiple, curve goes from 0 - M
+    public int idea_k; // entry cost/barrier to enter an idea
 
     // data collecting variables
     public int total_effort; // Total amount of effort invested in idea to that point
