1fc8c8d0ac159aebf1696df8a60ac79e6d83f5b3
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-11 13:56:16 -0700

    model step updates

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.lang.Object;
+import java.lang.Math;
 import java.util.Random;
 
 public class Model {
@@ -9,7 +10,11 @@ public class Model {
 		time_periods = config.time_periods;
 		ideas_per_time = config.ideas_per_time;
 		tp_alive = config.tp_alive;
+		idea_mean = config.idea_mean;
+		idea_max = config.idea_max;
+		start_effort_mean = config.start_effort_mean;
 		k_mean = config.k_mean;
+		
     }
 	
 	public ArrayList<Scientist> scientists;
@@ -21,10 +26,22 @@ public class Model {
 	public int ideas_per_time;
 	public int tp_alive;
 	public int k_mean;
+	public int idea_mean;
+	public int idea_max;
+	public int start_effort_mean;
 	
     public void step() {
     	for(int i = 0; i < num_sci; i++) {
     		Scientist scientist = new Scientist();
+    		scientist.idea_max_var = getRandomNumber(idea_max);
     	}
     }
+    
+    public int getRandomNumber(int average) {
+		double x = Math.random();
+		int min = 0;
+		int max = average * 2;
+		int num = (int)(x * ((max - min) + 1)) + min;
+		return num;
+	}
 }
--- src/Run.java
+++ src/Run.java
@@ -2,8 +2,7 @@ public class Run {
     public static void main(String[] args) {
         Config config = new Config();
         Model model = new Model(config);
-
-        for (int i=0; i<config.time_periods; i++) {
+        for (int i = 0; i < config.time_periods; i++) {
             model.step();
         }
     }
