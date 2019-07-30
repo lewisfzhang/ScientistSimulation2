e04af9f02044d2ca86b73bf7ca85c6342d22e31c
Conrad Milhaupt <ctmilhaupt@gmail.com>
2019-07-30 11:43:12 -0700

    push

--- src/Model.java
+++ src/Model.java
@@ -7,7 +7,7 @@ class Model {
 
 	// SCALARS, all updated in model step
 	int num_sci;
-	
+
 	int ideas_per_time;
 	int tp = 0;
 
@@ -19,20 +19,20 @@ class Model {
 	// model constructor
 	Model(Config config) {
 		this.config = config;
-    }
-	
+	}
+
 	// defines the process for one time period within the model
-    void step() {
+	void step() {
 		// recompute random number of ideas and scientists born within this time period
 		num_sci = Functions.poisson(config.sci_rate, config);
 		ideas_per_time = Functions.poisson(config.ideas_per_time, config);
 
-    	age_scientists();
-    	birth_new_scientists();
+		age_scientists();
+		birth_new_scientists();
 
 		int ideas_last_tp = birth_new_ideas(); // keep track of how many old ideas so we only have to update new ideas
 		update_scientist_idea(ideas_last_tp);
-		
+
 		if (config.funding) {
 			distribute_funding();
 		}
@@ -41,43 +41,43 @@ class Model {
 			sci.step();
 		}
 
-    	update_objects();
-    	pay_out_returns();
+		update_objects();
+		pay_out_returns();
 
-    	tp++;
-    }
+		tp++;
+	}
 
 	// adds one year to the age of every scientist that already exists within the model
 	void age_scientists() {
-    	for (Scientist sci : scientist_list) {
-    		sci.age++;
+		for (Scientist sci : scientist_list) {
+			sci.age++;
 		}
-    }
+	}
 
-    // creates new scientists, birthed at age 0, and sets their random constants (variances, learning speed, and effort)
+	// creates new scientists, birthed at age 0, and sets their random constants (variances, learning speed, and effort)
 	void birth_new_scientists() {
-    	for(int s = 0; s < num_sci; s++) {
-    		Scientist sci = new Scientist(this);
-    		scientist_list.add(sci);
-    	}
-    	num_sci_tp.add(num_sci);
-    }
+		for(int s = 0; s < num_sci; s++) {
+			Scientist sci = new Scientist(this);
+			scientist_list.add(sci);
+		}
+		num_sci_tp.add(num_sci);
+	}
 
-    // creates new ideas and sets their random constants (true mean, true max, investment cost)
+	// creates new ideas and sets their random constants (true mean, true max, investment cost)
 	// returns the number of ideas from last tp
 	int birth_new_ideas() {
-    	for(int i = 0; i < ideas_per_time; i++) {
-    		Idea idea = new Idea(this);
-    		idea_list.add(idea);
-    	}
+		for(int i = 0; i < ideas_per_time; i++) {
+			Idea idea = new Idea(this);
+			idea_list.add(idea);
+		}
 		// return should be 0 if tp = 0 because there are no previous ideas before tp = 0 (see set_perceived_rewards)
-    	return (tp == 0) ? 0 : idea_list.size() - ideas_per_time;
-    }
+		return (tp == 0) ? 0 : idea_list.size() - ideas_per_time;
+	}
 
-    // loop through every scientist, appending their perceived rewards dataframe with new ideas
+	// loop through every scientist, appending their perceived rewards dataframe with new ideas
 	// also updates related list with extra spots for new ideas --> append_scientist_lists
 	void update_scientist_idea(int ideas_last_tp) {
-    	for (Scientist sci : scientist_list) {
+		for (Scientist sci : scientist_list) {
 			// determining how many loops we need to run (for performance efficiency)
 			// just born scientists need to update for all ideas --> sci.age = 0
 			// older scientists only need to update for new ideas
@@ -122,21 +122,21 @@ class Model {
 					count++;
 				}
 			}
-    	}
-    }
+		}
+	}
 
 	// updates the lists within each scientist object to reflect the correct number of available ideas
-    // ignore static warning, only because we aren't using self keyword
+	// ignore static warning, only because we aren't using self keyword
 	// keep it in model since it is called by the model step function --> set_perceived_rewards()
 	void append_scientist_lists(Scientist sci) {
-    	sci.idea_eff_tp.add(0.0);
-        sci.idea_eff_tot.add(0.0);
-        sci.ideas_k_paid_tp.add(0);
-        sci.ideas_k_paid_tot.add(0);
+		sci.idea_eff_tp.add(0.0);
+		sci.idea_eff_tot.add(0.0);
+		sci.ideas_k_paid_tp.add(0);
+		sci.ideas_k_paid_tot.add(0);
 		sci.discov_ideas.add(0);
 		sci.returns_tp.add(0.0);
-        sci.returns_tot.add(0.0);
-    }
+		sci.returns_tot.add(0.0);
+	}
 
 	// pays out grant money from the budget in each period before 
 	void distribute_funding() {
@@ -153,120 +153,132 @@ class Model {
 				scientists_alive++;
 			}
 		}
-		
+
 		int total_budget = (int) (config.budget_prop * scientists_alive * config.start_effort_mean);
-			double total_young_k = config.prop_young_k * total_budget;
-			double total_young_e = config.prop_young_e * total_budget;
-			double total_old_k = config.prop_old_k * total_budget;
-			double total_old_e = config.prop_old_e * total_budget;
-			
-			while(total_young_e > 0) {
-				if(young_sci.size() > 0) {
-					double e_grant = total_young_e / (config.recipient_prop * young_sci.size());
-					int recipient = Functions.get_random_int(0, young_sci.size(), config);
-					Scientist sci = young_sci.get(recipient);
-					sci.e_funding = e_grant;
-					total_young_e -= e_grant;
-					young_sci.remove(recipient);
-				}
+		double total_young_k = config.prop_young_k * total_budget;
+		double total_young_e = config.prop_young_e * total_budget;
+		double total_old_k = config.prop_old_k * total_budget;
+		double total_old_e = config.prop_old_e * total_budget;
+
+		while(total_young_e > 0) {
+			double e_grant = total_young_e / (config.recipient_prop * young_sci.size());
+			if(young_sci.size() > 0) {
+				int recipient = Functions.get_random_int(0, young_sci.size(), config);
+				Scientist sci = young_sci.get(recipient);
+				sci.e_funding = e_grant;
+				total_young_e -= e_grant;
+				young_sci.remove(recipient);
 			}
-			while(total_young_k > 0) {
-				if(young_sci.size() > 0) {
-					double k_grant = total_young_k / (config.recipient_prop * young_sci.size());
-					int recipient = Functions.get_random_int(0, young_sci.size(), config);
-					Scientist sci = young_sci.get(recipient);
-					sci.k_funding = k_grant;
-					total_young_k -= k_grant;
-					young_sci.remove(recipient);
-				}
-			}
-			while(total_old_e > 0) {
-				if(old_sci.size() > 0) {
-					double e_grant = total_old_e / (config.recipient_prop * old_sci.size());
-					int recipient = Functions.get_random_int(0, old_sci.size(), config);
-					Scientist sci = old_sci.get(recipient);
-					sci.e_funding = e_grant;
-					total_old_e -= e_grant;
-					old_sci.remove(recipient);
-				}
-			}
-			while(total_old_k > 0) {
-				if(old_sci.size() > 0) {
-					double k_grant = total_old_k / (config.recipient_prop * old_sci.size());
-					int recipient = Functions.get_random_int(0, old_sci.size(), config);
-					Scientist sci = old_sci.get(recipient);
-					sci.k_funding = k_grant;
-					total_old_k -= k_grant;
-					old_sci.remove(recipient);
-				}
+			if(total_young_e < e_grant) {
+				break;
 			}
 		}
-	
+		while(total_young_k > 0) {
+			double k_grant = total_young_k / (config.recipient_prop * young_sci.size());
+			if(young_sci.size() > 0) {
+				int recipient = Functions.get_random_int(0, young_sci.size(), config);
+				Scientist sci = young_sci.get(recipient);
+				sci.k_funding = k_grant;
+				total_young_k -= k_grant;
+				young_sci.remove(recipient);
+			}
+			if(total_young_k < k_grant) {
+				break;
+			}
+		}
+		while(total_old_e > 0) {
+			double e_grant = total_old_e / (config.recipient_prop * old_sci.size());
+			if(old_sci.size() > 0) {
+				int recipient = Functions.get_random_int(0, old_sci.size(), config);
+				Scientist sci = old_sci.get(recipient);
+				sci.e_funding = e_grant;
+				total_old_e -= e_grant;
+				old_sci.remove(recipient);
+			}
+			if(total_young_e < e_grant) {
+				break;
+			}
+		}
+		while(total_old_k > 0) {
+			double k_grant = total_old_k / (config.recipient_prop * old_sci.size());
+			if(old_sci.size() > 0) {
+				int recipient = Functions.get_random_int(0, old_sci.size(), config);
+				Scientist sci = old_sci.get(recipient);
+				sci.k_funding = k_grant;
+				total_old_k -= k_grant;
+				old_sci.remove(recipient);
+			}
+			if(total_young_k < k_grant) {
+				break;
+			}
+		}
+	}
+
 	// data collection: loop through each idea object, updating the effort that was invested in this time period
 	void update_objects() {
-    	for(int idx = 0; idx < idea_list.size(); idx++) {
-    		Idea idea = idea_list.get(idx);
-    		double effort_invested_tp = 0;
-    		int k_paid_tp = 0; // number of scientists who learned the idea in this tp
+		for(int idx = 0; idx < idea_list.size(); idx++) {
+			Idea idea = idea_list.get(idx);
+			double effort_invested_tp = 0;
+			int k_paid_tp = 0; // number of scientists who learned the idea in this tp
 
-    		for (Scientist sci : scientist_list) {
-    			effort_invested_tp += sci.idea_eff_tp.get(idx);
-    			k_paid_tp += sci.ideas_k_paid_tp.get(idx);
-    		}
+			for (Scientist sci : scientist_list) {
+				effort_invested_tp += sci.idea_eff_tp.get(idx);
+				k_paid_tp += sci.ideas_k_paid_tp.get(idx);
+			}
 
-    		idea.total_effort += effort_invested_tp;
-    		idea.num_k_total += k_paid_tp;
-    		idea.effort_by_tp.add(effort_invested_tp);
-    		idea.num_k_by_tp.add(k_paid_tp);
-    	}
-    }
+			idea.total_effort += effort_invested_tp;
+			idea.num_k_total += k_paid_tp;
+			idea.effort_by_tp.add(effort_invested_tp);
+			idea.num_k_by_tp.add(k_paid_tp);
+		}
+	}
 
-    // determine who gets paid out based on the amount of effort input
+	// determine who gets paid out based on the amount of effort input
 	void pay_out_returns() {
-    	for(int i = 0; i < idea_list.size(); i++) {
-    		Idea idea = idea_list.get(i);
-    		if(idea.effort_by_tp.get(tp) > 0) {
-    			double start_effort = idea.total_effort - idea.effort_by_tp.get(tp); // since we already updated effort_by_tp in update_objects()
-    			double end_effort = idea.total_effort;
-    			double idea_returns = Idea.get_returns(idea.idea_mean, idea.idea_sds, idea.idea_max, start_effort, end_effort);
-    			process_winners(i, idea_returns); // process the winner for each idea, one per loop
-    		}
-    	}
+		for(int i = 0; i < idea_list.size(); i++) {
+			Idea idea = idea_list.get(i);
+			if(idea.effort_by_tp.get(tp) > 0) {
+				double start_effort = idea.total_effort - idea.effort_by_tp.get(tp); // since we already updated effort_by_tp in update_objects()
+				double end_effort = idea.total_effort;
+				double idea_returns = Idea.get_returns(idea.idea_mean, idea.idea_sds, idea.idea_max, start_effort, end_effort);
+				process_winners(i, idea_returns); // process the winner for each idea, one per loop
+			}
+		}
 
-    	for (Scientist sci : scientist_list) {
-    		double tp_returns = sci.returns_tp.stream().mapToDouble(i -> i).sum(); // sum of sci.returns_tp
-    		sci.overall_returns_tp.add(tp_returns);
-    	}
-    }
+		for (Scientist sci : scientist_list) {
+			double tp_returns = sci.returns_tp.stream().mapToDouble(i -> i).sum(); // sum of sci.returns_tp
+			sci.overall_returns_tp.add(tp_returns);
+		}
+	}
 
-    // processes winners for idea with index iidx
+	// processes winners for idea with index iidx
 	void process_winners(int iidx, double idea_returns) {
-    	ArrayList<Integer> list_of_investors = new ArrayList<>();
-    	for(int s = 0; s < scientist_list.size(); s++) {
-    		Scientist sci = scientist_list.get(s);
-    		if(sci.idea_eff_tp.get(iidx) != 0) {
-    			list_of_investors.add(s);
-    		}
-    	}
+		ArrayList<Integer> list_of_investors = new ArrayList<>();
+		for(int s = 0; s < scientist_list.size(); s++) {
+			Scientist sci = scientist_list.get(s);
+			if(sci.idea_eff_tp.get(iidx) != 0) {
+				list_of_investors.add(s);
+			}
+		}
 
-    	if(config.equal_returns) { // each scientist receives returns proportional to effort
-    		double total_effort_invested = 0; // double so we can do "double" division in 2nd for loop
-    		for (int sci_id : list_of_investors) {
-    			Scientist sci = scientist_list.get(sci_id);
-    			total_effort_invested += sci.idea_eff_tp.get(iidx);
-    		}
+		if(config.equal_returns) { // each scientist receives returns proportional to effort
+			double total_effort_invested = 0; // double so we can do "double" division in 2nd for loop
+			for (int sci_id : list_of_investors) {
+				Scientist sci = scientist_list.get(sci_id);
+				total_effort_invested += sci.idea_eff_tp.get(iidx);
+			}
 			for (int sci_id : list_of_investors) { // must be separate for loop from above since we need to calculate total_effort_invested first
 				Scientist sci = scientist_list.get(sci_id);
-    			double individual_proportion = sci.idea_eff_tp.get(iidx) / total_effort_invested;
-    			double individual_returns = individual_proportion * total_effort_invested;
-    			Functions.arr_increment_double(sci.returns_tp, iidx, individual_returns);
-    			Functions.arr_increment_double(sci.returns_tot, iidx, individual_returns);
-    		}
-    	} else {
-    		int oldest_scientist_id = list_of_investors.get(0); // scientist born "earliest" in same tp should come first in list
-    		Scientist sci = scientist_list.get(oldest_scientist_id);
-    		Functions.arr_increment_double(sci.returns_tp, iidx, idea_returns);
+				double individual_proportion = sci.idea_eff_tp.get(iidx) / total_effort_invested;
+				double individual_returns = individual_proportion * total_effort_invested;
+				Functions.arr_increment_double(sci.returns_tp, iidx, individual_returns);
+				Functions.arr_increment_double(sci.returns_tot, iidx, individual_returns);
+			}
+		} else {
+			int oldest_scientist_id = list_of_investors.get(0); // scientist born "earliest" in same tp should come first in list
+			Scientist sci = scientist_list.get(oldest_scientist_id);
+			Functions.arr_increment_double(sci.returns_tp, iidx, idea_returns);
 			Functions.arr_increment_double(sci.returns_tot, iidx, idea_returns);
-    	}
-    }
+		}
+	}
 }
